db1bcd74cb70f266646cd80465915d2e1ddb738b
mluzpereztrazos <mluzperez18@alumnostrazos.net>
2025-08-03 19:16:52 +0200

    Add files via upload

---
+++ Plantilla react.jsx
@@ -0,0 +1,12 @@
+import Button from './components/button/Button';
+
+const App = () => {
+  return (
+    <>
+      <h1>React y Node </h1>
+      <Button />
+    </>
+  );
+};
+
+export default App;
